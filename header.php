837719efa2b64ae646879b3be4513cf8757007b1
MasakaChika <winx0419@gmail.com>
2024-05-08 09:59:48 +0900

    GA導入 20240508

--- header.php
+++ header.php
@@ -6,6 +6,18 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="description" content="<?php bloginfo('description'); ?>" />
 	<?php wp_head(); ?>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-NYGK6WLEF4"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-NYGK6WLEF4');
+	</script>
 </head>
 
 <body <?php body_class(); ?>>
