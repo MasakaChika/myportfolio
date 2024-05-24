7989ea9b511275086f2968dba4bfe8c0eea370e7
MasakaChika <winx0419@gmail.com>
2024-05-24 22:15:59 +0900

    アクセシビリティの改善 20240524

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,57 @@
 /* ------- mv ------- */
 // テキストアニメーションを適用する関数（アニメーション時間の引数を追加）
 function applyAnimationToText(selector, staggerTime) {
+  // prefers-reduced-motionメディアクエリをチェック
+  const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+
   // 対象の要素を取得
   const paragraph = document.querySelector(selector);
 
-  // テキストコンテンツを取得して一時的なDIVに挿入し、テキストと<br>を処理
-  const tempDiv = document.createElement('div');
-  tempDiv.innerHTML = paragraph.innerHTML;
-  const newTextContent = Array.from(tempDiv.childNodes)
-    .map((node) => {
-      if (node.nodeType === Node.TEXT_NODE) {
-        return [...node.nodeValue].map((char) => `<span>${char === ' ' ? '&nbsp;' : char}</span>`).join('');
-      } else if (node.nodeType === Node.ELEMENT_NODE && node.tagName === 'BR') {
-        return '<br>';
-      }
-      return ''; // ノードタイプが対象外の場合、空文字列を返す
-    })
-    .join('');
+  if (!prefersReducedMotion) {
+    // モーションアニメーションが有効の場合の処理
+    // テキストコンテンツを取得して一時的なDIVに挿入し、テキストと<br>を処理
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = paragraph.innerHTML;
+    const newTextContent = Array.from(tempDiv.childNodes)
+      .map((node) => {
+        if (node.nodeType === Node.TEXT_NODE) {
+          return [...node.nodeValue].map((char) => `<span>${char === ' ' ? '&nbsp;' : char}</span>`).join('');
+        } else if (node.nodeType === Node.ELEMENT_NODE && node.tagName === 'BR') {
+          return '<br>';
+        }
+        return ''; // ノードタイプが対象外の場合、空文字列を返す
+      })
+      .join('');
 
-  // 新しい文字列をHTMLに挿入
-  paragraph.innerHTML = newTextContent;
+    // 新しい文字列をHTMLに挿入
+    paragraph.innerHTML = newTextContent;
 
-  // gsapアニメーションの適用（staggerTimeを使用）
-  gsap.fromTo(
-    `${selector} span`,
-    {
-      autoAlpha: 0,
-    },
-    {
-      autoAlpha: 1,
-      stagger: {
-        each: staggerTime,
-        from: 'random',
+    // gsapアニメーションの適用（staggerTimeを使用）
+    gsap.fromTo(
+      `${selector} span`,
+      {
+        autoAlpha: 0,
       },
-    }
-  );
+      {
+        autoAlpha: 1,
+        stagger: {
+          each: staggerTime,
+          from: 'random',
+        },
+      }
+    );
+  }
 }
 
 // 関数を使用してアニメーションを適用（異なるアニメーション時間を指定）
 document.addEventListener('DOMContentLoaded', function () {
+  applyAnimationToText('.mv__msg', 0.032); // .mv__msgの時間
+  applyAnimationToText('.mv__sitename', 0.23); // .mv__sitenameの時間
+});
+
+// prefers-reduced-motionメディアクエリの状態が変化した際にも処理を実行するようにイベントリスナーを追加
+window.matchMedia('(prefers-reduced-motion: reduce)').addEventListener('change', function () {
+  // モーションアニメーションの有効性が変更された際に再度処理を実行
   applyAnimationToText('.mv__msg', 0.032); // .mv__msgの時間
   applyAnimationToText('.mv__sitename', 0.23); // .mv__sitenameの時間
 });
