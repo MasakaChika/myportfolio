042550a5355475da997955208ad3f4791591190d
MasakaChika <winx0419@gmail.com>
2024-06-09 21:58:27 +0900

    アクセシビリティ修正 20240609

--- functions/admin.php
+++ functions/admin.php
@@ -5,7 +5,7 @@
  *------------------------------------------*/
 function remove_menus()
 {
-	remove_menu_page('edit.php'); // 投稿
+	// remove_menu_page('edit.php'); // 投稿
 	remove_menu_page('edit-comments.php'); // コメント
 }
 add_action('admin_menu', 'remove_menus', 999);
--- functions/setting.php
+++ functions/setting.php
@@ -164,7 +164,47 @@ function cptui_register_my_cpts_work()
 }
 add_action('init', 'cptui_register_my_cpts_work');
 
+/*------------------------------------------
+ *  Post Type: journal.
+ *----------------------------------------*/
+function cptui_register_my_cpts_journal()
+{
+	$labels = [
+		"name" => esc_html__("journal", "custom-post-type-ui"),
+		"singular_name" => esc_html__("journal entry", "custom-post-type-ui"),
+	];
 
+	$args = [
+		"label" => esc_html__("Journal", "custom-post-type-ui"),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"rest_namespace" => "wp/v2",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"can_export" => false,
+		"rewrite" => ["slug" => "journal", "with_front" => true],
+		"query_var" => true,
+		"menu_icon" => "dashicons-welcome-write-blog",
+		"supports" => ["title", "editor", "thumbnail", "excerpt", "trackbacks", "custom-fields", "revisions"],
+		"taxonomies" => ["category", "post_tag"], // journal用のカテゴリとタグを指定
+		"show_in_graphql" => false,
+	];
+
+	register_post_type("journal", $args);
+}
+add_action('init', 'cptui_register_my_cpts_journal');
 
 /*------------------------------------------
  * 特定のカスタム投稿タイプのアーカイブページである場合、そのカスタム投稿タイプの複数形ラベル（label）をタイトルとして使用
